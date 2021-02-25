f06063f704206e3d5cf7dfbf068ec18be345d065
labossieremarc <labossieremarc@gmail.com>
2021-02-25 10:36:14 -0700

    End of Section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,7 +52,6 @@ namespace API
             app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
