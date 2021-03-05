6ac16d0400bddb50caf258d82688e054b820c224
labossieremarc <labossieremarc@gmail.com>
2021-03-05 13:54:00 -0700

    End of Section 15

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,6 +23,8 @@ namespace API.Entities
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> CurrentUserLikes { get; set; }
         public ICollection<UserLike> LikingOtherUsers { get; set;}
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
 
     }
 }
